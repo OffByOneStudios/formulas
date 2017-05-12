41b06118c74750e526ac853f81427ee2a2368172
Clark Rinker <cgrinker@gmail.com>
2017-05-12 09:42:36 -0700

    Fixing Formula binding.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 
 var parser = require('../peg/parser.js');
 
+function evaluator(argument) {
+  var val = argument;
+  if (this.isInv)
+  {
+    val *= this.ia;
+    val += this.ic;
+    val = 1/val;
+  }
+  val *= this.a;
+  val += this.c;
+  return val;
+}
+
 function evaluate(formula) {
   // TODO check for only 1 variable
   var formula_struct = parser.parse(formula.replace(/[a-zA-Z]+/, 'x'))
-  const evaluate = (val) => {
-    if (this.isInv)
-    {
-      val *= this.ia;
-      val += this.ic;
-      val = 1/val;
-    }
-    val *= this.a;
-    val += this.c;
-    return val;
-  };
-  let res =  evaluate.bind(formula_struct);
+  let res =  evaluator.bind(formula_struct);
   Object.defineProperty(res, 'formula', {value: () => { return formula_struct}})
   return res;
 }
